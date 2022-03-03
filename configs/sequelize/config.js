02f00f7e8a01fb8462b7a6a933de009bc026cc2f
FernandoDzay <luisf.dzay@gmail.com>
2022-03-02 22:53:30 -0600

    Preparando para produccion

---
+++ configs/sequelize/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  dev: {
+    username: process.env.DEV_USERNAME,
+    password: process.env.DEV_PASSWORD,
+    database: process.env.DEV_DATABASE,
+    host: process.env.DEV_HOST,
+    dialect: "mysql"
+  },
+  test: {
+    username: process.env.TEST_USERNAME,
+    password: process.env.TEST_PASSWORD,
+    database: process.env.TEST_DATABASE,
+    host: process.env.TEST_HOST,
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.PRODUCTION_USERNAME,
+    password: process.env.PRODUCTION_PASSWORD,
+    database: process.env.PRODUCTION_DATABASE,
+    host: process.env.PRODUCTION_HOST,
+    dialect: "mysql"
+  }
+};
