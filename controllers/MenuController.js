702479ed0389ad3d73ab1b89db386eb6b6a2c042
FernandoDzay <luisf.dzay@gmail.com>
2022-07-11 02:27:50 -0500

    puliendo detalles

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -18,7 +18,7 @@ module.exports = {
         const settingDiscount = await Setting.findOne({where: {setting: 'discount_price'}});
         const discount = settingDiscount ? settingDiscount.int_value : 0;
 
-        const menuResponse = menu.map(menu => ({id: menu.id, food: {...menu.food.get(), discount}}));
+        const menuResponse = menu.map(menu => ({id: menu.id, food: {...menu.food.get(), discount: menu.food.price - discount}}));
         return res.json({status: setting.int_value === 0 ? 1 : 2, menu: menuResponse});
     },
 
