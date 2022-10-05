603b5f663274f10ef1f3ff0d85393ba191892701
FernandoDzay <luisf.dzay@gmail.com>
2022-10-05 12:21:58 -0500

    Cambiando horario de cron

--- services/Crons.js
+++ services/Crons.js
@@ -1,11 +1,11 @@
 const cron = require('node-cron');
 const CronsController = require('../controllers/CronsController.js');
 
-cron.schedule('0 0 4 * * *', CronsController.checkForBirhdaysFunction);
+cron.schedule('0 0 2 * * *', CronsController.checkForBirhdaysFunction);
 
-cron.schedule('0 0 4 * * *', CronsController.dailyRemoveFunction);
+cron.schedule('0 0 2 * * *', CronsController.dailyRemoveFunction);
 
-cron.schedule('0 0 4 * * *', CronsController.dailyRotateGroupsFunction);
+cron.schedule('0 0 2 * * *', CronsController.dailyRotateGroupsFunction);
 
 
 // 0 0 6 * * ?  <-- Every day at 6am
