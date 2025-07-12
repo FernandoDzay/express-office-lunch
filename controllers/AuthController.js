b0cbe2f4fa26f52ce9ab0a15d1ee95d7cff1ace2
FernandoDzay <luisf.dzay@gmail.com>
2025-07-12 12:22:13 -0600

    agregando logs al login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -26,14 +26,23 @@ module.exports = {
         let match;
 
         try { user = await User.findOne({where: {email}}); }
-        catch(e) { return next(authError); }
+        catch(e) {
+            console.log(e);
+            return next(authError);
+        }
         if(user === null) return res.status(400).json({error: authIncorrect});
         try { match = await user.auth(password); }
-        catch(e) { return next(authError); }
+        catch(e) {
+            console.log(e);
+            return next(authError);
+        }
         
         if(!match) return res.status(400).json({error: authIncorrect});
         jwt.sign({ id: user.id }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'}, function(err, token) {
-            if(err) next(authError);
+            if(err) {
+                console.log(err);
+                next(authError);
+            }
             res.status(200).json({token});
         });
         return;
